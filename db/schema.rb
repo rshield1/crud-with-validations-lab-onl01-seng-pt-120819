14afb0be8430fc6a354ef90e3629aec693d45390
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-05 20:01:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,13 @@
 ActiveRecord::Schema.define(version: 20200505191122) do
 
   create_table "songs", force: :cascade do |t|
-    t.string  "title"
-    t.boolean "released"
-    t.integer "release_year"
-    t.string  "artist_name"
+    t.string   "title"
+    t.integer  "release_year"
+    t.boolean  "released"
+    t.string   "genre"
+    t.string   "artist_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
